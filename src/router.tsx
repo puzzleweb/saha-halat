abf91f22e3e614badc04b0b94b8f51913e73ddbe
Nadi <djktasarim@gmail.com>
2026-06-16 18:03:34 +0300

    ee

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath: "/saha-halat/",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    base: "/saha-halat/",
+  },
+  nitro: {
+    preset: 'github-pages'
+  },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
