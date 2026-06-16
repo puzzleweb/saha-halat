9dca5e95ba2e686681b66dcf75f4cd4f1df31451
Nadi <djktasarim@gmail.com>
2026-06-16 20:15:15 +0300

    ee

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -535,11 +535,17 @@ function Contact() {
           </form>
         </div>
 
-        <div className="mt-12 lg:mt-16 aspect-[21/9] w-full bg-white/5 border border-white/10 flex items-center justify-center">
-          <div className="text-center text-white/40">
-            <MapPin className="h-8 w-8 mx-auto mb-2" />
-            <div className="text-xs uppercase tracking-wider">Harita / Konum</div>
-          </div>
+        <div className="mt-12 lg:mt-16 aspect-[21/9] w-full bg-white/5 border border-white/10 overflow-hidden">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3057.5017949202024!2d32.744711099999996!3d39.974890200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d3491c5f209ccf%3A0x4ecdd2892c5294a4!2sSaha%20Zincir%20%C3%87elik%20Halat%20%C4%B0malat%20Sanayi%20LTD.%20%C5%9ET%C4%B0.!5e0!3m2!1str!2str!4v1781629968130!5m2!1str!2str"
+            width="100%"
+            height="100%"
+            style={{ border: 0, filter: "grayscale(1) invert(0.9) hue-rotate(180deg)" }}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Saha Zincir Konum"
+          />
         </div>
       </div>
     </section>
